152c1f27a925351fff967b2b8cacbb9f4d6ffde6
Achmad Syarifromadoni <114554492+Achsyarifromadoni@users.noreply.github.com>
2023-03-08 09:53:22 +0700

    Create index.php

---
+++ latihan01/index.php
@@ -0,0 +1,27 @@
+<?php
+    print_r("okay");
+    print("Syarif");
+    echo "<b>Hello worlds</b>";
+    $nama = "Achmad";
+    $nama2 = "Syarif";
+    echo $nama." ".$nama2;
+    // atau
+    // echo ($nama) ($nama2);
+
+    $nilaiquis  = 60;
+    $nilaitugas = 70;
+    $hasilpenjumlahan = $nilaiquis + $nilaitugas;
+    $hasilpengurangan = $nilaiquis - $nilaitugas;
+    $hasilperkalian = $nilaiquis * $nilaitugas;
+    $hasilpembagian = $nilaiquis / $nilaitugas;
+    $hasileksponen = $nilaiquis ** $nilaitugas;
+    $hasilmodulus = $nilaiquis % $nilaitugas;
+    
+    echo "Penjumlahan $nilaiquis + $nilaitugas = $hasilpenjumlahan <br>";
+    echo "Pengurangan $nilaiquis - $nilaitugas = $hasilpengurangan <br>";
+    echo "Perkalian $nilaiquis * $nilaitugas = $hasilperkalian <br>";
+    echo "Pembagian $nilaiquis / $nilaitugas = $hasilpembagian <br>";
+    echo "eksponen $nilaiquis ** $nilaitugas = $hasileksponen <br>";
+    echo "modulus $nilaiquis % $nilaitugas = $hasilmodulus <br>";
+    
+?>
