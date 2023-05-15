59d4437fc2863de8e2520995a5d4cb295eedabb9
Achmad Syarifromadoni <114554492+Achsyarifromadoni@users.noreply.github.com>
2023-05-15 13:48:18 +0700

    Add files via upload

--- Latihan02/Punya Lain/admin/Mod_kategori/index.php
+++ Latihan02/Punya Lain/admin/Mod_kategori/index.php
@@ -28,19 +28,22 @@
                     </tr>
 
                     <?php
+                    //$no = 1;
                     while($row = mysqli_fetch_array($qdata)){
                     ?>
 
                     <tr>
+                        <!-- <td><?php //echo $no; ?></td> -->
                         <td><?php echo $row['id_kategori']?></td>
                         <td><?php echo $row['nm_kategori']?></td>
                         <td> 
-                            <button class="btn btn-primary" type="submit"><i class="bi bi-pencil-saquare">Edit</i></button>
-                            <button class="btn btn-primary" type="submit"><i class="bi bi-x-lg">Delete</i></button>
+                            <a href="?modul=Mod_kategori&aksi=edit&id=<?php echo $row['id_kategori']?>"><button class="btn btn-primary" type="submit"><i class="bi bi-pencil-saquare">Edit</i></button></a>
+                            <a href="mod_kategori/proses.php?proses=delete&id=<?php echo $row['id_kategori']?>"><button class="btn btn-primary" type="submit"><i class="bi bi-x-lg">Delete</i></button></a>
                         </td>
                     </tr>
 
-                    <?php } 
+                    <?php //$no++; 
+                    } 
                     
                     ?>
                     
@@ -53,6 +56,23 @@
     
     <?php }
     elseif(isset($_GET['aksi'])){
+        if($_GET['aksi'] == "edit"){
+            //Proses Edit
+            $query = mysqli_query($koneksidb, "select * from mst_kategori where id_kategori=".$_GET['id']."") 
+            or die ("Data Tidak ditemukan".mysqli_error($koneksidb));
+
+            $data = mysqli_fetch_array($query);
+            // echo $data['nm_kategori'];
+            $nama = $data['nm_kategori'];
+            $exproses = "update";
+        }
+        else if($_GET['aksi'] == "add"){
+            $nama = "";
+            $exproses = "insert";
+            $idnya = 0;
+            $nama = "";
+            //Proses Add
+        }
     ?>
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
 
@@ -60,17 +80,20 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css"> -->
 
-<form action="mod_kategori/proses.php?aksi=insert" method="post">
+<form action="mod_kategori/proses.php?proses=<?php echo $exproses; ?>" method="post">
 <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3">
-            </div>
-            <div class="col-md">
-            <font face = "Comic sans MS" size =" 5">From Input Data</font>
-                <div class="mb-3 row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Nama Kategori</label>
-                    <div class="col-sm-7">
-                        <input type="text" name="txt_nama" id="txt_nama" class="form-control" id="inputkategori">
+            
+            <div class="col-md-mb-4 row">
+            <font face = "Comic sans MS" size =" 5" style="margin-left: 100px;">From Input Data</font>
+                <div class="mb-4 row">
+                    <label for="inputPassword" class="col-sm-4 col-form-label">Nama Kategori</label>
+                    <div class="col-sm-6">
+                        
+                        <input type="hidden" name="txt_id" id="txt_id" value="<?php echo $idnya; ?>">
+                        
+                        <input type="text" name="txt_nama" id="txt_nama" class="form-control" id="inputkategori" value= <?php echo $nama?>>
+                        <div class="dropdown-divider"></div>
                         
                         <button class="btn btn-secondary" type="reset"><i class="bi bi-x-lg"> Batal</i></button>
                         <button class="btn btn-primary" type="submit"><i class="bi bi-save"> Simpan</i></button>
@@ -86,4 +109,6 @@
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
 
     <?php } ?>
+
+    
  
--- Latihan02/Punya Lain/admin/Mod_kategori/proses.php
+++ Latihan02/Punya Lain/admin/Mod_kategori/proses.php
@@ -1,22 +1,46 @@
 <?php 
 require_once("../../koneksidb.php");
 
-if($_GET['aksi'] == 'insert'){
+if($_GET['proses'] == 'insert'){
     echo "Proses Tambah";
     $nm_kategori = $_POST['txt_nama'];
     $exsave = mysqli_query($koneksidb, "insert into mst_kategori(nm_kategori) values('".$nm_kategori."')")
     or die("Gagal Simpan" .mysqli_error($koneksidb));
     if($exsave){
         // ketik proses simpan berhasil
-        header("Location: http://localhost/Mathul_webprog/latihan02/Punya%20Lain/admin/mod_kategori/");
+        header("Location: http://localhost/Mathul_webprog/latihan02/Punya%20Lain/admin/home.php?modul=Mod_kategori");
     }
 
 }
-else if ($_GET['aksi'] == "update"){
+else if ($_GET['proses'] == "update"){
     echo "Proses Update";
+    $namakategori = $_GET['txt_nama'];
+    $idkategori = $_POST['txt_id'];
+    $exsave = mysqli_query($koneksidb,
+    "UPDATE mst_kategori SET nm_kategori='$namakategori'
+    WHERE id_kategori=$idkategori")
+    or die ("gagal update" .mysqli_error($koneksidb));
     
+    
+
+if ($exsave){
+    //Ketik Proses Simpan berhasil
+    header("Location: http://localhost/Mathul_webprog/latihan02/Punya%20Lain/admin/home.php?modul=Mod_kategori");
 }
-else if ($_GET['aksi'] == "delete"){
+}
+
+else if ($_GET['proses'] == "delete"){
     echo "Proses Delete";
+    $id = $_GET['id'];
+    $exsave = mysqli_query($koneksidb,
+        "delete from mst_kategori WHERE id_kategori=$id")
+    or die ("gagal update" .mysqli_error($koneksidb));
+        
+        
+
+if ($exsave){
+    //Ketik Proses Simpan berhasil
+    header("Location: http://localhost/Mathul_webprog/latihan02/Punya%20Lain/admin/home.php?modul=Mod_kategori");
+}
 }
 ?>
